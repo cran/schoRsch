f65e8ce7918ba2511483474c23150bcf69db3549
Unknown author <unknown@unknown>
2014-07-30 00:00:00 +0000

    version 1.1

--- R/anova_out.R
+++ R/anova_out.R
@@ -34,22 +34,35 @@ anova_out <- function(ezout,
   # (2) Assemble ANOVA table
   # ---------------------------------------------
   # Construct table
+  doeswork <- 1;
   if ("ANOVA" %in% names(x)) {
-    outtable <- data.frame(
-      Effect=x$ANOVA$Effect,
-      MSE=x$ANOVA$SSd/x$ANOVA$DFd,
-      df1=x$ANOVA$DFn,
-      df2=x$ANOVA$DFd,
-      F=format(round(x$ANOVA$F,2),nsmall=2),
-      p=format(round(x$ANOVA$p,3),nsmall=3),
-      petasq=format(round(x$ANOVA$SSn/
-        (x$ANOVA$SSn+x$ANOVA$SSd),2),nsmall=2),
-      getasq=format(round(x$ANOVA$ges,2),nsmall=2)
-      );
+    # Check whether SSn and SSd are present in input data
+    if ("SSn" %in% colnames(x$ANOVA) && "SSd" %in% colnames(x$ANOVA)) {
+      outtable <- data.frame(
+        Effect=x$ANOVA$Effect,
+        MSE=x$ANOVA$SSd/x$ANOVA$DFd,
+        df1=x$ANOVA$DFn,
+        df2=x$ANOVA$DFd,
+        F=format(round(x$ANOVA$F,2),nsmall=2),
+        p=format(round(x$ANOVA$p,3),nsmall=3),
+        petasq=format(round(x$ANOVA$SSn/
+          (x$ANOVA$SSn+x$ANOVA$SSd),2),nsmall=2),
+        getasq=format(round(x$ANOVA$ges,2),nsmall=2)
+        );
+    } else {
+      outtable <- "Couldn't find Sum of Squares in ezANOVA output. Please use the 'detailed=TRUE' option!";
+      doeswork <- 0;
+    }
   } else {
-    outtable <- "N/A ... possibly wrong input?" 
+    outtable <- "N/A ... possibly wrong input?";
+    doeswork <- 0;
   }
   
+  # Do remaining operations only if main output could be created
+  if (doeswork < 1) {
+    print(outtable);
+  } else { 
+    
   # ---------------------------------------------
   # (3) Sphericity tests
   # ---------------------------------------------  
@@ -163,4 +176,5 @@ anova_out <- function(ezout,
   if (print==TRUE) {
     print(x);
   }
+  } # Do only if doeswork > 0
 }
