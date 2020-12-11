564d2d2cc70067f38ecf18b9c82dd68c72bcaa8b
Roland Pfister <roland.pfister@psychologie.uni-wuerzburg.de>
2020-12-11 18:30:02 +0000

    version 1.9

--- R/toclipboard.R
+++ R/toclipboard.R
@@ -8,12 +8,19 @@ toclipboard <- function(data,
                   na = "NA",
                   dec = ".",
                   row.names = FALSE,
-                  col.names = TRUE) {
+                  col.names = TRUE,
+                  clipwarning = FALSE) {
   
   if (tolower(.Platform$OS.type)=="windows") {
-  write.table(data, file = "clipboard", sep = sep,
-              quote = quote, eol = eol, na = na, dec = dec,
-              row.names = row.names, col.names = col.names)
+    tryCatch({
+      write.table(data, file = "clipboard", sep = sep,
+                  quote = quote, eol = eol, na = na, dec = dec,
+                  row.names = row.names, col.names = col.names)},
+      warning = function(cond){
+        write.table("Buffer overflow! Couldn't copy all content to clipboard. Set argument clipwarning = FALSE to fill buffer with incomplete data.", file = "clipboard",
+                    row.names = FALSE, col.names = FALSE)
+      }
+    )
   } else {
   print("The operating system you are using is currently not supported by 'toclipboard'. No output has been written.")
   }
